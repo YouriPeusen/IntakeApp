6a6fda3359d9eccda1c02c36e97a33e61b6072fa
Judith <33284318+darksideofpotato@users.noreply.github.com>
2021-06-22 11:59:00 +0200

    User API

---
+++ ArticleApis/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ArticleApis.Models;
+using IntakeApp.Repository;
+using IntakeApp.Repository.Models;
+
+namespace ArticleApis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserContext _context;
+
+        public UserController(UserContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            using (var contextt = new b2d_4_4_intakeapp_dbDbContext())
+            {
+                return await contextt.Users.ToListAsync();
+            }
+
+        }
+
+    }
+}
