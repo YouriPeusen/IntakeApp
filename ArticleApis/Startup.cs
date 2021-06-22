6958ca1058cb173828811de50ed09b6f2bc819e2
Youri Peusen <youri-peusen@hotmail.com>
2021-06-22 15:33:17 +0100

    Startpage en API fix

--- ArticleApis/Startup.cs
+++ ArticleApis/Startup.cs
@@ -47,6 +47,8 @@ namespace ArticleApis
 
             app.UseAuthorization();
 
+            app.UseDefaultFiles(new DefaultFilesOptions { DefaultFileNames = new List<string> { "index.html" } });
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
