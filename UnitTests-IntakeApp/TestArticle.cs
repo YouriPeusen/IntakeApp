e1430f33433d064def18a39c8475f6cd757e96b9
Youri Peusen <youri-peusen@hotmail.com>
2021-06-29 03:05:30 +0100

    Article Unittest gemaakt

--- UnitTests-IntakeApp/TestArticle.cs
+++ UnitTests-IntakeApp/TestArticle.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IntakeApp.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +8,21 @@ namespace UnitTests_IntakeApp
 {
 	class TestArticle
 	{
+		[TestMethod]
+		public void TestFailing()
+		{
+			Assert.IsFalse(true);
+		}
 
+		[TestMethod]
+		public void TestCreate()
+		{
+			Article article = new Article(1, 3, 5, 7, 9, new DateTime(2021, 6, 15), new DateTime(2021, 7, 15), "ImageString.jpg", "Comment");
+			Assert.AreEqual(1, article.getId());
+			Assert.AreEqual(3, article.getProductId());
+			Assert.AreEqual(5, article.getStatusId());
+			Assert.AreEqual(7, article.getProviderId());
+			Assert.AreEqual(9, article.getRenterId());
+		}
 	}
 }
