cd6002696849bb77455c2412292636c085f29d70
Judith <33284318+darksideofpotato@users.noreply.github.com>
2021-06-29 11:59:31 +0200

    begin fix backend

--- IntakeApp/Pages/Index.cshtml.cs
+++ IntakeApp/Pages/Index.cshtml.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using IntakeApp.Classes;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Globalization;
 
 namespace IntakeApp.Pages
 {
@@ -61,33 +62,42 @@ namespace IntakeApp.Pages
 		public void OnPostInput()
 		{
 
-			// maak object van class op basis van invoerveld, doe vervolgens code uit dal daarmee
-			//DateTime dateOne = DateTime.ParseExact("06/04/2021 10:00", "MM/dd/yyyy HH:mm", CultureInfo.InvariantCulture);
-			//DateTime dateTwo = DateTime.ParseExact("06/04/2021 10:00", "MM/dd/yyyy HH:mm", CultureInfo.InvariantCulture);
+            //maak object van class op basis van invoerveld, doe vervolgens code uit dal daarmee
 
-			//Article newArtcile = new Article(0, Convert.ToInt32(productChooser.SelectedValue), Convert.ToInt32(statusChooser.SelectedValue), Convert.ToInt32(userChooser.SelectedValue), 0, dateOne, dateTwo, articleImage0.Value, commentary.Value);
-			//dal.AddNewArticle(newArtcile);
+            DateTime dateOne = DateTime.ParseExact("06/04/2021 10:00", "MM/dd/yyyy HH:mm", CultureInfo.InvariantCulture);
+			DateTime dateTwo = DateTime.ParseExact("06/04/2021 10:00", "MM/dd/yyyy HH:mm", CultureInfo.InvariantCulture);
 
-			// Het onderdeel dat bij het aanmaken van een nieuw artikel de punten bijschrijft bij de gebruiker. Via de ingevoerde categorie zoekt hij
-			// de bijbehorende punten. Hierna wordt de gebruiker gezocht op basis van de keuze. De punten uit de categorie worden bij deze gebruiker
-			// bijgeschreven. 
+			int chosenProduct = Convert.ToInt32(Request.Form["Ddl_Products"]);
+			int chosenUser = Convert.ToInt32(Request.Form["Ddl_User"]);
 
-			//int categoryId = Convert.ToInt32(categoryChooser.SelectedValue);
+
+            Article newArtcile = new Article(0, chosenProduct, 1, chosenUser, 0, dateOne, dateTwo, "test", "test");
+			dal.AddNewArticle(newArtcile);
+
+			 //Het onderdeel dat bij het aanmaken van een nieuw artikel de punten bijschrijft bij de gebruiker.Via de ingevoerde categorie zoekt hij
+             //de bijbehorende punten. Hierna wordt de gebruiker gezocht op basis van de keuze. De punten uit de categorie worden bij deze gebruiker
+             //bijgeschreven. 
+
+
+   //         int categoryId = Convert.ToInt32(categoryChooser.SelectedValue);
 			//Category fromCategory = dal.GetCategoryDetails(categoryId);
 			//int rewardedPoints = fromCategory.GetPoints();
 
-			// Zoekt het userId op en voegt de punten toe bij de functie in de DAL. Het opzoeken van de user doet hij ook automatisch in deze
-			// functie.
-			//int userId = Convert.ToInt32(userChooser.SelectedValue);
+			//Zoekt het userId op en voegt de punten toe bij de functie in de DAL.Het opzoeken van de user doet hij ook automatisch in deze
+			//functie.
+
+		 //  int userId = Convert.ToInt32(userChooser.SelectedValue);
 			//dal.AddPointsToUser(userId, rewardedPoints);
-		}
+			}
 
 		public void OnPostAddProduct()
 		{
-			// maak object van class op basis van invoerveld, doe vervolgens code uit dal daarmee
+			//maak object van class op basis van invoerveld, doe vervolgens code uit dal daarmee
+			System.Diagnostics.Debug.WriteLine("hoi");
 
-			//Product newProduct = new Product(0, Convert.ToInt32(categoryChooser0.SelectedValue), productName.Value, productDescriptionNA.Value);
-			//dal.AddNewProduct(newProduct);
+			int chosenCategory = Convert.ToInt32(Request.Form["Ddl_Category"]);
+            Product newProduct = new Product(0, chosenCategory, "test", "test");
+        dal.AddNewProduct(newProduct);
 		}
 	}
 }
